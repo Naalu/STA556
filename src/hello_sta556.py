3c68a1e9866fd9b548e4bb336b033b18f214379f
Chris Reger <59154050+Naalu@users.noreply.github.com>
2026-09-01 22:13:33 +0000

    Complete Week 1 workflow activity

---
+++ src/hello_sta556.py
@@ -0,0 +1,12 @@
+import sys
+
+python_version = sys.version_info
+
+semester = "Fall 2026"
+name = "Chris"
+
+print(f"Hello from STA 556 ({semester})!")
+
+print(f"Welcome to the course, {name}.")
+
+print(f"You are using Python version {python_version.major}.{python_version.minor}.{python_version.micro}.")
