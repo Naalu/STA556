af022c0af259228355933a33ce622adc5af776d6
Chris Reger <59154050+Naalu@users.noreply.github.com>
2026-09-01 22:16:46 +0000

    Add workflow message to Week 1 script

--- src/hello_sta556.py
+++ src/hello_sta556.py
@@ -10,3 +10,7 @@ print(f"Hello from STA 556 ({semester})!")
 print(f"Welcome to the course, {name}.")
 
 print(f"You are using Python version {python_version.major}.{python_version.minor}.{python_version.micro}.")
+
+print(
+    "I am beginning to build reproducible computational workflows."
+)
